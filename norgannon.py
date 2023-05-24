89969a89d93152840db4ffbe875a554cb63d88b3
BattleNub89 <joecharp89@gmail.com>
2023-05-23 19:32:09 -0500

    Initial commit

---
+++ norgannon.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+
+import discord
+from discord.ext import commands
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = discord.Client(intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+bot.run(token)
